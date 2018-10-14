2a8ddac32f236c4e60a453c10e263b3a424b9ef5
k-nasa <htilcs1115@gmail.com>
2018-10-14 14:18:09 +0900

    update version

--- rmemo.rb
+++ rmemo.rb
@@ -1,14 +1,14 @@
 class Rmemo < Formula
-  desc "CLI memo tool"
-  homepage "https://github.com/k-nasa/rmemo"
-  url "https://github.com/k-nasa/rmemo/releases/download/0.1.2/rmemo-v0.1.2-x86_64-apple-darwin.zip"
-  version "0.1.2"
-  sha256 "101d54714a5082297ebd19317dfe31ade5cdeac4ed0eba04df9f7cb76a3e016b"
+  desc 'CLI memo tool'
+  homepage 'https://github.com/k-nasa/rmemo'
+  url 'https://github.com/k-nasa/rmemo/releases/download/0.1.3/rmemo-v0.1.3-x86_64-apple-darwin.zip'
+  version '0.1.3'
+  sha256 'b91e75e26b3cf7d1e3a5ac9e69c68e5061230511cd790eb290dafa5cfc61bf22'
 
-  depends_on "fzf"
+  depends_on 'fzf'
 
   def install
-    bin.install "rmemo"
+    bin.install 'rmemo'
   end
 
   test do
