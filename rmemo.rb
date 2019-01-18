1bd96c5f041b46d3252ed11955003af38809f46a
nasa <htilcs1115@gmail.com>
2019-01-18 23:57:35 +0900

    Update rmemo.rb

--- rmemo.rb
+++ rmemo.rb
@@ -1,9 +1,8 @@
 class Rmemo < Formula
   desc 'CLI memo tool'
   homepage 'https://github.com/k-nasa/rmemo'
-  url 'https://github.com/k-nasa/rmemo/releases/download/0.1.3/rmemo-v0.1.3-x86_64-apple-darwin.zip'
-  version '0.1.3'
-  sha256 'b91e75e26b3cf7d1e3a5ac9e69c68e5061230511cd790eb290dafa5cfc61bf22'
+  url 'https://github.com/k-nasa/rmemo/releases/download/v0.2.5/rmemo_memo-v0.2.5-x86_64-apple-darwin.zip'
+  version '0.2.5'
 
   depends_on 'fzf'
 
