a52ef2fca6e73e64c623809fbfc4d327ee90634b
nasa <htilcs1115@gmail.com>
2019-01-19 00:02:10 +0900

    fix: sha1 -> sha256

--- rmemo.rb
+++ rmemo.rb
@@ -3,7 +3,7 @@ class Rmemo < Formula
   homepage 'https://github.com/k-nasa/rmemo'
   url 'https://github.com/k-nasa/rmemo/releases/download/v0.2.5/rmemo_memo-v0.2.5-x86_64-apple-darwin.zip'
   version '0.2.5'
-  sha1 '44cc39da63e4f0b214a53b40f0d9ce631948fa62f4550580c6c20f94d7eb3ea2'
+  sha256 '44cc39da63e4f0b214a53b40f0d9ce631948fa62f4550580c6c20f94d7eb3ea2'
 
   depends_on 'fzf'
 
