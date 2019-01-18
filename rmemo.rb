673e80141703c5d1c2a2df282812b641429955de
nasa <htilcs1115@gmail.com>
2019-01-19 00:07:37 +0900

    add sha256 (必須みたい)

--- rmemo.rb
+++ rmemo.rb
@@ -3,6 +3,7 @@ class Rmemo < Formula
   homepage 'https://github.com/k-nasa/rmemo'
   url 'https://github.com/k-nasa/rmemo/releases/download/v0.2.5/rmemo-v0.2.5-x86_64-apple-darwin.zip'
   version '0.2.5'
+  sha256 '77c7a1f97290e7deb582cc733b2cbaa48ed2c27de75acf8bda461d9bd0d17aa1'
 
   depends_on 'fzf'
 
