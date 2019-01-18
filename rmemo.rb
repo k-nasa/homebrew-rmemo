385b2f138c033b0a10caafdb85f20f13f437c8b4
nasa <htilcs1115@gmail.com>
2019-01-19 00:05:44 +0900

    fix: source url

--- rmemo.rb
+++ rmemo.rb
@@ -1,7 +1,7 @@
 class Rmemo < Formula
   desc 'CLI memo tool'
   homepage 'https://github.com/k-nasa/rmemo'
-  url 'https://github.com/k-nasa/rmemo/releases/download/v0.2.5/rmemo_memo-v0.2.5-x86_64-apple-darwin.zip'
+  url 'https://github.com/k-nasa/rmemo/releases/download/v0.2.5/rmemo-v0.2.5-x86_64-apple-darwin.zip'
   version '0.2.5'
   sha256 '44cc39da63e4f0b214a53b40f0d9ce631948fa62f4550580c6c20f94d7eb3ea2'
 
